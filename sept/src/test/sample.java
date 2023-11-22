d450949d7a9b3cb5bf9f87d9391e36e270823f97
admin <admin@DESKTOP-1RDOGHK>
2023-11-22 21:59:20 +0530

    test

---
+++ sept/src/test/sample.java
@@ -0,0 +1,20 @@
+package test;
+
+public class sample {    
+	
+	static int a=30;
+	static int b=10;
+	
+
+	public static void main(String[] args) {
+		
+		int Sum=a-b;
+      System.out.println(Sum); 
+      
+      
+      
+      
+      
+	}
+
+}
